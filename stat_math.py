40d263f07dedc47c132644cfbbeb85df9a4fa1ac
Aner-sh <anersharan@gmail.com>
2025-09-08 16:37:21 +0300

    make funcs

--- stat_math.py
+++ stat_math.py
@@ -1,1 +1,18 @@
-#We will win
+#- get_sd(data: List[int])
+#- get_varience(data: List[int])
+#- get_average(data: List[int])
+
+def get_sd()
+{
+
+}
+
+def get_varience()
+{
+  
+}
+
+def get_average()
+{
+
+}
