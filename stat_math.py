fe90a3a101b141167247d133b9524f138da41364
Aner-sh <anersharan@gmail.com>
2025-09-08 16:44:26 +0300

    function bodies updated

--- stat_math.py
+++ stat_math.py
@@ -2,17 +2,19 @@
 #- get_varience(data: List[int])
 #- get_average(data: List[int])
 
-def get_sd()
-{
+import math
 
-}
+def get_average(data):
+    if not data:
+        return 0.0
+    return sum(data) / len(data)
 
-def get_varience()
-{
-  
-}
+def get_variance(data):
+    if not data:
+        return 0.0
+    mean = get_average(data)
+    return sum((x - mean) ** 2 for x in data) / len(data)
 
-def get_average()
-{
-
-}
+def get_sd(data):
+    variance = get_variance(data)
+    return math.sqrt(variance)
