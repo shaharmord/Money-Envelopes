3a75bebc31f50629ddb1ae5658a56b41075cf90a
shaharmord <shaharmo6613@gmail.com>
2025-09-08 16:45:34 +0300

    Update envelope.py

--- envelope.py
+++ envelope.py
@@ -1,3 +1,17 @@
 class Envelope:
+    def __init__(self, value: int, next_envelope=None):
+        self._value = value
+        self._next = next_envelope
+        self._kept = False
+    
+  def get_value():
+    return self._value()
+
+  def keep():
+    self._kept =True
+    return self._value()
+
+  def toss_and_get_next():
+    return self._next()
   
-  
+    
